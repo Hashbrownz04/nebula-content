4f54e5d7a2f1ad54da4a7d9fa06b50542362e2ef
Hashbrownz04 <cruzhashem@gmail.com>
2024-03-19 14:07:27 -0400

    created another js file_updated19Mar24-14h07

--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/variable-notes.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/variable-notes.js
@@ -6,5 +6,19 @@ const Number = 404;
 console.log(String);
 console.log(Number);
 
+String = "Dolor Sit Amit";
+
+console.log(String);
+
 const fName = 'Hashem';
 console.log(fName);
+
+const color1 = 'red'
+{
+    console.log(color1);
+}
+
+{
+    const color2 = 'yellow';
+    console.log(color2);
+}
