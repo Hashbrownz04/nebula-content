e29fbcb0577fe283df3143bbd404efa6752f4db5
Hashbrownz04 <cruzhashem@gmail.com>
2024-03-19 12:19:00 -0400

    created another js file_19Mar24-12h18

--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/data-type-practice.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/data-type-practice.js
@@ -1,3 +1,2 @@
 
 console.log("This is my first log")
-
---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/variable-notes.js
@@ -0,0 +1,10 @@
+
+let String = "Lorem Ipsum";
+
+const Number = 404;
+
+console.log(String);
+console.log(Number);
+
+const fName = 'Hashem';
+console.log(fName);
