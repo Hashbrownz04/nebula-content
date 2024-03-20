b1a644981cf5601e51007752d15d70b51974d44b
Hashbrownz04 <cruzhashem@gmail.com>
2024-03-20 12:39:10 -0400

    began numbers-practice work_20Mar24-12h36

--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Numbers-Practice-1.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Numbers-Practice-1.js
@@ -8,19 +8,19 @@ console.log(number);
 // Create two numbers, assign them to variables and add them together
  let x = 2;
  let y = 4;
- console.log(x+y);
+ console.log(x + y);
 
 // Create three numbers, assign them to variables and subtract the smaller two from the largest
 let fNumber = 2;
 let scndNumber = 4;
 let thrdNumber = 8;
-thrdNumber - (fNumber+scndNumber);
+thrdNumber - (fNumber + scndNumber);
 console.log(thrdNumber);
 
 // Create two even numbers, assign them to variables and divide one from the other
 let input = 4;
 let divide = 2;
-console.log(input/divide);
+console.log(input / divide);
 
 // Create two odd numbers, assign them to variables and multiply one from the other
 let input2 = 3;
@@ -29,7 +29,7 @@ console.log(input * multiply);
 
 // Create a variable and log its remainder, if divided by 3, to the console 
 let oddNumber = 13;
-console.log(oddNumber%5);
+console.log(oddNumber % 5);
 
 // Answer in plain text, what is modulo operator especially useful for?
 console.log("The modulus operator \"\%\" is used to find the remainder from the quotient.")
