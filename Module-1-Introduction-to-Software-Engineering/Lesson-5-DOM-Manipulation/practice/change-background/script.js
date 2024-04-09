95106854b5b071a1cf5483fc056da7de2604ce67
Hashbrownz04 <cruzhashem@gmail.com>
2024-04-09 03:08:55 -0400

    linked HTML with CSS and JavaScript_09Apr24-3h08

--- Module-1-Introduction-to-Software-Engineering/Lesson-5-DOM-Manipulation/practice/change-background/script.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-5-DOM-Manipulation/practice/change-background/script.js
@@ -1,39 +1,38 @@
-const square = document.querySelector('.square');
+const background = document.querySelector('body');
+const square = document.querySelector('.square')
 let clicked = 0;
 
 square.addEventListener("click", (event) => {
     if(event.target == square && clicked == 0){
-        square.style.backgroundColor = "orange";
+        background.style.backgroundColor = "orange";
         clicked = 1;
     }
     else if(event.target == square && clicked == 1){
-        square.style.backgroundColor = "yellow";
+        background.style.backgroundColor = "yellow";
         clicked = 2;
     }
     else if(event.target == square && clicked == 2){
-        square.style.backgroundColor = "green";
+        background.style.backgroundColor = "green";
         clicked = 3;
     }
     else if(event.target == square && clicked == 3){
-        square.style.backgroundColor = "blue";
+        background.style.backgroundColor = "blue";
         clicked = 4;
     }
     else if(event.target == square && clicked == 4){
-        square.style.backgroundColor = "purple";
+        background.style.backgroundColor = "purple";
         clicked = 5;
     }
     else if(event.target == square && clicked == 5){
-        square.style.backgroundColor = "black";
-        square.style.color = "white";
+        background.style.backgroundColor = "black";
         clicked = 6;
     }
     else if(event.target == square && clicked == 6){
-        square.style.backgroundColor = "white";
-        square.style.color = "black";
+        background.style.backgroundColor = "white";
         clicked = 7;
     }
     else if(event.target == square && clicked == 7){
-        square.style.backgroundColor = "red";
+        background.style.backgroundColor = "red";
         clicked = 0;
     }
 })
